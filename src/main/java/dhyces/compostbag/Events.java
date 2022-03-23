cdc4d7def85629e78b835aac7d102b205fe02a8e
dhyces <pokmon987@gmail.com>
2022-03-23 01:04:57 -0400

    Fix ClientEvents
    -Removed getToCancelTooltips
    -Fixed the cancelOtherTooltips event listener
    -we now just check that the item is an instanceof in renderTooltipWhileHovering
    -Reduced tick requirements on the Ticker

--- src/main/java/dhyces/compostbag/Events.java
+++ src/main/java/dhyces/compostbag/Events.java
@@ -1,54 +1,36 @@
 package dhyces.compostbag;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.lwjgl.glfw.GLFW;
 
 import dhyces.compostbag.item.CompostBagItem;
-import dhyces.compostbag.tooltip.ClientCompostBagTooltip;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMaps;
+import dhyces.compostbag.tooltip.CompostBagTooltip;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
-import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
-import net.minecraft.network.protocol.game.ServerboundContainerClickPacket;
-import net.minecraft.network.protocol.game.ServerboundSetCreativeModeSlotPacket;
 import net.minecraft.world.entity.SlotAccess;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ClickAction;
-import net.minecraft.world.inventory.ClickType;
-import net.minecraft.world.inventory.ContainerSynchronizer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ContainerScreenEvent;
 import net.minecraftforge.client.event.RenderTooltipEvent;
-import net.minecraftforge.event.TickEvent.ClientTickEvent;
-import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @OnlyIn(Dist.CLIENT)
-@EventBusSubscriber(modid = CompostBag.MODID)
+@EventBusSubscriber(modid = CompostBag.MODID, value = Dist.CLIENT)
 public class Events {
-	
-	@SubscribeEvent
-	static void getToCancelTooltips(final RenderTooltipEvent event) {
-		
-	}
 
 	@SubscribeEvent
 	static void cancelOtherTooltips(final RenderTooltipEvent.GatherComponents event) {
-		if (event.getTooltipElements().get(0).right().map(c -> c instanceof ClientCompostBagTooltip).orElse(false)) {
+		if (event.getTooltipElements().get(1).right().map(c -> c instanceof CompostBagTooltip).orElse(false)) {
 			return;
 		}
 		var mc = Minecraft.getInstance();
-		if (mc.screen.getFocused() instanceof RecipeBookComponent && mc.screen instanceof AbstractContainerScreen<?> screen) {
+		if (mc.screen.children().stream().anyMatch(c -> c instanceof RecipeBookComponent) && mc.screen instanceof InventoryScreen screen) {
 			if (screen.getMenu().getCarried().getItem() instanceof CompostBagItem && Screen.hasControlDown()) {
 				event.setCanceled(true);
 			}
@@ -59,7 +41,7 @@ public class Events {
 	static void renderTooltipWhileHovering(final ContainerScreenEvent.DrawForeground e) {
 		var clientPlayer = Minecraft.getInstance().player;
 		var carried = clientPlayer.inventoryMenu.getCarried();
-		if (!Screen.hasControlDown() || carried.isEmpty() || !carried.is(CompostBag.RegistryEvents.COMPOST_BAG))
+		if (!Screen.hasControlDown() || carried.isEmpty() || !(carried.getItem() instanceof CompostBagItem))
 			return;
 		
 		var screen = e.getContainerScreen();
@@ -67,7 +49,7 @@ public class Events {
 		screen.renderTooltip(pose, screen.getTooltipFromItem(carried), carried.getTooltipImage(), e.getMouseX()-screen.getGuiLeft(), e.getMouseY()-screen.getGuiTop(), carried);
 	}
 	
-	static final Ticker TICKER = new Ticker(50, 40);
+	static final Ticker TICKER = new Ticker(40, 30);
 	
 	@SubscribeEvent
 	static void multiDrop(final ContainerScreenEvent event) {
@@ -93,19 +75,6 @@ public class Events {
 				}
 			}
 		}
-	}
-	
-	private static SlotAccess createSlotAccess(AbstractContainerScreen<?> container) {
-		return new SlotAccess() {
-			public ItemStack get() {
-				return container.getMenu().getCarried();
-			}
-			
-			public boolean set(ItemStack p_150452_) {
-				container.getMenu().setCarried(p_150452_);
-				return true;
-			}
-		};
 	}
 	
 	static class Ticker {
