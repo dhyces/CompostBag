02e61a863ec5292a19760a849f4b9fe775194c33
dhyces <pokmon987@gmail.com>
2022-04-03 19:52:47 -0400

    Move ticker to separate common class

--- Forge/src/main/java/dhyces/compostbag/Events.java
+++ Forge/src/main/java/dhyces/compostbag/Events.java
@@ -1,5 +1,6 @@
 package dhyces.compostbag;
 
+import dhyces.compostbag.util.Ticker;
 import org.lwjgl.glfw.GLFW;
 
 import com.mojang.blaze3d.platform.InputConstants;
@@ -87,36 +88,6 @@ public class Events {
 					screen.mouseReleased(x, y, 1);
 				}
 			}
-		}
-	}
-
-	static class Ticker {
-		int ticks;
-		final int first;
-		final int rest;
-
-		/** On the first interaction, it should be a longer delay before more items are input.
-		 * The rest should be fairly short, but enough delay to see items disappear.*/
-		public Ticker(int firstInteract, int rest) {
-			this.first = firstInteract;
-			this.rest = rest;
-		}
-
-		/** Returns whether or not this tick should interact*/
-		public boolean tick() {
-			ticks++;
-			if (ticks <= first) {
-				return ticks % first == 0;
-			}
-			if (ticks % rest == 0) {
-				ticks = first;
-				return true;
-			}
-			return false;
-		}
-
-		public void restart() {
-			ticks = 0;
 		}
 	}
 }
