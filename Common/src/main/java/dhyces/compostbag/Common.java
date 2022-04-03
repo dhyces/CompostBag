e2147e5edaf34f197c5d5741dc57bac5a7330d15
dhyces <pokmon987@gmail.com>
2022-04-03 19:55:29 -0400

    Remove comments and unused method callback. Also add item and registration

--- Common/src/main/java/dhyces/compostbag/Common.java
+++ Common/src/main/java/dhyces/compostbag/Common.java
@@ -1,35 +1,20 @@
 package dhyces.compostbag;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import dhyces.compostbag.item.CompostBagItem;
+import dhyces.compostbag.platform.Services;
+import net.minecraft.core.Registry;
+import net.minecraft.world.item.Item;
 
-import java.util.List;
+import java.util.function.Supplier;
 
 public class Common {
 
-    // This method serves as an initialization hook for the mod. The vanilla
-    // game has no mechanism to load tooltip listeners so this must be
-    // invoked from a mod loader specific project like Forge or Fabric.
+    public static final Supplier<Item> COMPOST_BAG_ITEM;
+
     public static void init() {
     }
 
-    // This method serves as a hook to modify item tooltips. The vanilla game
-    // has no mechanism to load tooltip listeners so this must be registered
-    // by a mod loader like Forge or Fabric.
-    public static void onItemTooltip(ItemStack stack, TooltipFlag context, List<Component> tooltip) {
-
-        if (!stack.isEmpty()) {
-
-            final FoodProperties food = stack.getItem().getFoodProperties();
-
-            if (food != null) {
-
-                tooltip.add(new TextComponent("Nutrition: " + food.getNutrition()));
-                tooltip.add(new TextComponent("Saturation: " + food.getSaturationModifier()));
-            }
-        }
+    static {
+        COMPOST_BAG_ITEM = Services.PLATFORM.registerItem(Registry.ITEM, "compost_bag", () -> new CompostBagItem());
     }
 }
