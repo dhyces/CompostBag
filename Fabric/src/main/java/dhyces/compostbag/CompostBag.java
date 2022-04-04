1969f66e5769e57890d15c300179f161baf321cd
dhyces <pokmon987@gmail.com>
2022-04-04 13:15:33 -0400

    CompostBag now holds the ticker

--- Fabric/src/main/java/dhyces/compostbag/CompostBag.java
+++ Fabric/src/main/java/dhyces/compostbag/CompostBag.java
@@ -1,11 +1,23 @@
 package dhyces.compostbag;
 
+import dhyces.compostbag.util.Ticker;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
-public class CompostBag implements ModInitializer {
+public class CompostBag implements ModInitializer, ClientModInitializer {
     
     @Override
     public void onInitialize() {
         Common.init();
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static final Ticker TICKER = new Ticker(10, 5);
+
+    @Override
+    public void onInitializeClient() {
+
     }
 }
