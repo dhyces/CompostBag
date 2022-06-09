ca21eee04c3aead63d5af3ad823ba3e4928577eb
dhyces <pokmon987@gmail.com>
2022-06-09 14:02:32 -0400

    Switch Forge deferred register to take the ITEMS key

--- Forge/src/main/java/dhyces/compostbag/Registry.java
+++ Forge/src/main/java/dhyces/compostbag/Registry.java
@@ -6,7 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Registry {
-	public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MOD_ID);
+	public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.Keys.ITEMS, Constants.MOD_ID);
 
 	public static void register(IEventBus bus) {REGISTER.register(bus);}
 }
