47db44b873a7acaab2ba8e68d6df7f20ce1b25d5
dhyces <pokmon987@gmail.com>
2022-06-15 10:19:49 -0400

    Setup common logger and modLoc method

--- Common/src/main/java/dhyces/compostbag/Common.java
+++ Common/src/main/java/dhyces/compostbag/Common.java
@@ -3,7 +3,10 @@ package dhyces.compostbag;
 import dhyces.compostbag.item.CompostBagItem;
 import dhyces.compostbag.platform.Services;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.function.Supplier;
 
@@ -11,7 +14,13 @@ public class Common {
 
     public static final Supplier<Item> COMPOST_BAG_ITEM;
 
+    public static final Logger LOGGER = LogManager.getLogger(Constants.MOD_ID);
+
     public static void init() {
+    }
+
+    public static ResourceLocation modLoc(String id) {
+        return new ResourceLocation(Constants.MOD_ID, id);
     }
 
     static {
