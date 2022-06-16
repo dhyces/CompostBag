8d15222dba7a5a080a2e6abe7acf284e9b5a5d13
dhyces <pokmon987@gmail.com>
2022-06-15 23:37:49 -0400

    Added FABRIC QOL methods and syncing config
    Syncing works on server join, server disconnect, and server reload. On server join, the client's values are overridden by the server. On server disconnect, the config is reloaded on the client side and the values are set. On server reload, the PlayerListMixin#reloadResources method broadcasts the new server values to every connected client.

--- Fabric/src/main/java/dhyces/compostbag/CompostBagClient.java
+++ Fabric/src/main/java/dhyces/compostbag/CompostBagClient.java
@@ -2,11 +2,22 @@ package dhyces.compostbag;
 
 import dhyces.compostbag.item.CompostBagItem;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.event.Event;
 import net.minecraft.client.renderer.item.ItemProperties;
 
 public class CompostBagClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ItemProperties.register(Common.COMPOST_BAG_ITEM.get(), Common.modLoc("filled"), CompostBagItem::getFullnessDisplay);
+        ClientPlayNetworking.registerGlobalReceiver(Common.modLoc("config_sync"), (client, handler, buf, responseSender) -> {
+            var maxBonemeal = buf.readInt();
+
+            client.execute(() -> CompostBag.MAX_BONEMEAL = maxBonemeal);
+        });
+        ClientPlayConnectionEvents.DISCONNECT.register(Event.DEFAULT_PHASE, (handler, client) -> {
+            client.execute(() -> CompostBag.reloadConfig());
+        });
     }
 }
