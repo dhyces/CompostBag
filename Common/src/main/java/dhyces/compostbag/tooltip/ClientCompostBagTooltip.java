ed0e9fa3fe18a8002c6bfa912ae3456ea9457719
dhyces <pokmon987@gmail.com>
2023-03-15 15:40:55 -0400

    Port to 1.19.4

--- Common/src/main/java/dhyces/compostbag/tooltip/ClientCompostBagTooltip.java
+++ Common/src/main/java/dhyces/compostbag/tooltip/ClientCompostBagTooltip.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dhyces.compostbag.Constants;
 import dhyces.compostbag.item.CompostBagItem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
@@ -44,36 +45,34 @@ public class ClientCompostBagTooltip implements ClientTooltipComponent {
 
 	@Override
 	public void renderText(Font pFont, int pX, int pY, Matrix4f pMatrix4f, BufferSource pBufferSource) {
-		var text = countText();
-		pFont.drawInBatch(text, pX + getWidth(pFont) - pFont.width(text), pY+13, count == 0 ? 0x999999 : 0xFFFFFF, false, pMatrix4f, pBufferSource, false, 0, 0xFF);
+		String text = countText();
+		pFont.drawInBatch(text, pX + getWidth(pFont) - pFont.width(text), pY + 13, count == 0 ? 0x999999 : 0xFFFFFF, false, pMatrix4f, pBufferSource, Font.DisplayMode.NORMAL, 0, 0xFF, false);
 	}
 
 	private String countText() {
 		return count + "/" + CompostBagItem.MAX_BONEMEAL_COUNT.get();
 	}
 
-
 	@Override
-	public void renderImage(Font font, int x, int y, PoseStack pose, ItemRenderer renderer, int blitOffset) {
-		this.blitBin(pose, x, y, blitOffset);
-		this.blitFill(pose, x, y, blitOffset);
+	public void renderImage(Font font, int x, int y, PoseStack pose, ItemRenderer renderer) {
+		this.blitBin(pose, x, y);
+		this.blitFill(pose, x, y);
 
 		int center = getWidth(font)-10 - (font.width(countText()) / 2);
 
-		renderer.renderGuiItem(bonemeal, x+center, y-3);
+		renderer.renderGuiItem(pose, bonemeal, x+center, y-3);
 	}
 
-	private void blitFill(PoseStack pose, int x, int y, int offset) {
+	private void blitFill(PoseStack pose, int x, int y) {
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 	    RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
-	    var renderLevel = level*2;
-	    GuiComponent.blit(pose, x, y, offset, 0, 16, 16, 16, 128, 128);
-	    GuiComponent.blit(pose, x, y, offset, 0, 0, 16, 16-renderLevel, 128, 128);
+	    int renderLevel = level*2;
+	    GuiComponent.blit(pose, x, y+14-renderLevel, 0, 16, 16, renderLevel, 128, 128);
 	}
 
-	private void blitBin(PoseStack pose, int x, int y, int offset) {
+	private void blitBin(PoseStack pose, int x, int y) {
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 	    RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
-	    GuiComponent.blit(pose, x, y, offset, 0, 0, 16, 16, 128, 128);
+	    GuiComponent.blit(pose, x, y, 0, 0, 16, 16, 128, 128);
 	}
 }
