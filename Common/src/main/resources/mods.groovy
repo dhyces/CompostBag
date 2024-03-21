47acaf09d52ef0b5c64fa6a7a58eddce165c80c2
dhyces <10985914+dhyces@users.noreply.github.com>
2024-03-21 19:55:27 -0400

    Update to MDG 2 beta

--- Common/src/main/resources/mods.groovy
+++ Common/src/main/resources/mods.groovy
@@ -1,64 +1,42 @@
-import modsdotgroovy.Dependency
-
-ModsDotGroovy.make {
-    def modid = this.buildProperties["mod_id"]
+MultiplatformModsDotGroovy.make {
+    def modid = buildProperties["mod_id"]
 
     modLoader = "javafml"
-    loaderVersion = "${this.buildProperties["loader_version_range"]}"
+    loaderVersion = "${buildProperties["loader_version_range"]}"
 
     license = "MIT"
     issueTrackerUrl = "https://github.com/dhyces/CompostBag/issues"
     sourcesUrl = "https://github.com/dhyces/CompostBag"
 
-    onForge {
-        updateJsonUrl = "https://github.com/dhyces/CompostBag/raw/info/update.json"
-    }
-
     mod {
         modId = modid
-        displayName = this.buildProperties["mod_name"]
-        version = this.version
-        group = this.group
-        authors = [this.buildProperties["mod_author"] as String]
+        displayName = buildProperties["mod_name"]
+        version = buildProperties["version"]
+        authors = [buildProperties["mod_author"] as String]
 
         displayUrl = "https://www.curseforge.com/minecraft/mc-mods/compost-bag"
         logoFile = "assets/compostbag/logo.png"
         description = "Adds a bag that composts items like the composter and works like the bundle"
 
-        onFabric {
-            entrypoints {
-                main = "dev.dhyces.compostbag.CompostBag"
-                client = "dev.dhyces.compostbag.CompostBagClient"
-            }
+        entrypoints {
+            main "dev.dhyces.compostbag.FabricCompostBag"
+            client "dev.dhyces.compostbag.FabricCompostBagClient"
         }
 
         dependencies {
-            onForge { // TODO: add this back
-//                minecraft = "${this.buildProperties["minecraft_version_range"]}"
-                neoforge = "${this.buildProperties["neo_version_range"]}"
+            minecraft = "${buildProperties["minecraft_version_range"]}"
+            onNeoForge {
+                mod("neoforge") {
+                    versionRange = "${buildProperties["neo_version_range"]}"
+                }
             }
 
             onFabric {
-                minecraft = "${this.buildProperties["minecraft_version_range"]}"
-                fabricloader = ">=${this.fabricLoaderVersion}"
-                mod {
-                    modId = 'fabric-api'
-                    versionRange = '>=0.91.0'
+                mod("fabricloader") {
+                    versionRange = ">=${buildProperties["fabric_loader_version"]}"
                 }
-            }
-        }
-
-        onForge {
-            dependencies = dependencies.collect { dep ->
-                new Dependency() {
-                    @Override
-                    Map asForgeMap() {
-                        def map = dep.asForgeMap()
-                        def mand = map.get('mandatory')
-                        map.remove('mandatory')
-                        map.put('type', mand ? 'required' : 'optional')
-                        return map
-                    }
+                mod("fabric-api") {
+                    versionRange = ">=${(buildProperties["fabric_version"] as String).split("\\+")[0]}"
                 }
             }
         }
@@ -66,8 +44,14 @@ ModsDotGroovy.make {
 
     onFabric {
         environment = "*"
-        mixin = [
-                modid + ".mixins.json"
-        ]
+        mixins {
+            mixin("${modid}.mixins.json")
+        }
+    }
+
+    onNeoForge {
+        accessTransformers {
+            accessTransformer("META-INF/accesstransformer.cfg")
+        }
     }
 }
